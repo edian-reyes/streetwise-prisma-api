fcd17b0893e4589adc5a38060c86af30b6276e99
edian-reyes <edian.reyes92@gmail.com>
2019-01-28 23:08:01 -0400

    Test: fetch users in radar.io

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,17 @@
+const rp = require("request-promise");
+
 const users = async (parent, args, ctx, info) => {
+  const options = {
+    uri: "https://api.radar.io/v1/users",
+    headers: {
+      Authorization: process.env.RADAR_TEST_SECRET_KEY
+    },
+    json: true
+  };
+
+  const res = await rp(options);
+  console.log(res);
+
   return await ctx.db.query.users();
 };
 
